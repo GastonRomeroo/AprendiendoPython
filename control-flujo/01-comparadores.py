c28b9c3a064842aacb35a316121e3a1e1733d312
Gaston Romero <romero_gaston_@hotmail.com>
2023-05-04 23:34:29 -0300

    aprendiendo python

---
+++ control-flujo/01-comparadores.py
@@ -0,0 +1,9 @@
+print( 1 > 2)
+print( 1 < 2)
+print( 1 >= 2)
+print( 1 <= 2)
+print( 2 <= 2)
+print( 2 == 2)
+print( 2 == int("2")) #True
+print( 2 != 2)
+ 
---
+++ control-flujo/02-if.py
@@ -0,0 +1,13 @@
+edad = int(input("Ingrese su edad: "))
+#edad = 22
+if edad > 65:
+    print("Puede ver la película con un super descuento")
+elif edad > 55:
+    print("Puedes ver la pelicula con descuento")
+elif edad > 17:
+    print("Puedes ver la película")
+else:
+    print("No puedes entrar")
+print("Listo")
+
+
